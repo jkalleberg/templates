14dbccdf419db790110ce0739290ce239707e491
Jenna Kalleberg - Hellbender <jakth2@mail.missouri.edu>
2024-03-14 15:21:16 -0500

    breaking on 2nd run only

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -4,7 +4,12 @@ echo "=== demo.sh start > $(date)"
 echo "Our ${1} ${SCRIPT_TYPE} script worked!"
 sleep 10
 
-echo "Our ${1} ${SCRIPT_TYPE} script is still working!"
+if [[ $1 = "1st" ]]; then
+    echo "Our ${1} ${SCRIPT_TYPE} script is still working!"
+elif [[ $1 = "2nd" ]]; then
+    echo "ERROR: Our ${1} ${SCRIPT_TYPE} script is not working"
+    exit 1 # terminate and indicate error (any number but 0)
+fi
 sleep 10
 
 echo "=== demo.sh end > $(date)"
