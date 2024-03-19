a8e434f21227e04dbcc999eac2502ad431eb83bd
Jenna Kalleberg - Hellbender <jakth2@mail.missouri.edu>
2024-03-19 12:02:09 -0500

    creating a dir

--- scripts/experiments.sh
+++ scripts/experiments.sh
@@ -12,4 +12,16 @@ do
     echo -e "\tArgument #${itr}: ${i}"
 done
 
+# First argument should be a valid path to store SLURM jobs/logs
+echo "=== output path entered: ${1}"
+
+# Create that Output directory
+# -p creates any missing parent directories as well
+if [ ! -d $1 ]; then
+    echo "INFO: creating a new directory: ${1}"
+    mkdir -p $1
+else
+    echo "INFO: found existing path: ${1}"
+fi
+
 echo "=== experiments.sh end > $(date)"
