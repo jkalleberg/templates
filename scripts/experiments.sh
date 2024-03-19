36acd722ba1a2d81bb9331ae8e2c0af66333e30e
Jenna Kalleberg - Hellbender <jakth2@mail.missouri.edu>
2024-03-19 12:07:02 -0500

    adding default sub dirs

--- scripts/experiments.sh
+++ scripts/experiments.sh
@@ -13,15 +13,26 @@ do
 done
 
 # First argument should be a valid path to store SLURM jobs/logs
-echo "=== output path entered: ${1}"
+echo "=== output path entered | ${1}"
 
-# Create that Output directory
+# Create that output directory,
+# and create two directories to store SLURM jobs/logs
 # -p creates any missing parent directories as well
+
+sub_dirs=("jobs" "logs")
+
+echo "INFO: adding sub directories..."
+itr=0
+for dir in ${sub_dirs[@]}; do
+    itr=$((itr+1))
+    echo "INFO: creating sub dir ${itr} | ${dir}"
+done
+
 if [ ! -d $1 ]; then
-    echo "INFO: creating a new directory: ${1}"
+    echo "INFO: creating a new directory | ${1}"
     mkdir -p $1
 else
-    echo "INFO: found existing path: ${1}"
+    echo "INFO: found existing path | ${1}"
 fi
 
 echo "=== experiments.sh end > $(date)"
