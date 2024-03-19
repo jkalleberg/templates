61276c625a3f581a7a0183cfe1fcc0aea481a97b
Jenna Kalleberg - Hellbender <jakth2@mail.missouri.edu>
2024-03-19 13:22:29 -0500

    adding a demo mode which prints contents without submitting

--- scripts/experiments.sh
+++ scripts/experiments.sh
@@ -15,6 +15,13 @@ done
 # First argument should be a valid path to store SLURM jobs/logs
 echo "=== output path entered | ${1}"
 
+# Second argument == "demo" to print file contents, but not sumbit
+if [[ ${2} == "demo" ]]; then
+    export DEMO_MODE=true
+else
+    export DEMO_MODE=false
+fi
+
 # Create that output directory, if one does not exist
 # and create two directories to store SLURM jobs/logs
 # -p creates any missing parent directories as well
@@ -38,13 +45,20 @@ done
 jobs_dir="${1}/jobs"
 logs_dir="${1}/logs"
 
-sbatch_header="#!/bin/bash
+# Write MULTIPLE SBATCH files
+# Start with 10s then move up to 100s --> 1000s
+total_jobs=10
+
+for job_num in $(seq 1 $total_jobs); do
+    slurm_job_file="${jobs_dir}/demo_sbatch${job_num}.sh"
+    if [ ! -f $slurm_job_file ]; then
+        sbatch_header="#!/bin/bash
 #SBATCH --partition=general
 #SBATCH --nodes=1
 #SBATCH --mem=1G
 #SBATCH --time=0:20:00
 #SBATCH --account=schnabelr-lab
-#SBATCH --job-name=demo_sbatch
+#SBATCH --job-name=demo_sbatch${job_num}
 #SBATCH --output=${logs_dir}/%x_%j.out
 #SBATCH --mail-user=jakth2@mail.missouri.edu
 #SBATCH --mail-type=REQUEUE,FAIL,END
@@ -54,7 +68,7 @@ echo \"=== SBATCH running in: \${SLURM_SUBMIT_DIR}\"
 echo \"=== Memory Requested: \${SLURM_MEM_PER_NODE}\"
 "
 
-sbatch_contents="##-- SCIENCE GOES HERE -- ##
+        sbatch_contents="##-- SCIENCE GOES HERE -- ##
 export SCRIPT_TYPE=demo
 export MESSAGE='running demo.sh'
 export STATUS_FILE=${logs_dir}/tracker.txt
@@ -70,25 +84,38 @@ echo \"=== SBATCH IS STILL RUNNING \$(date)\"
 sleep 30
 echo \"=== SBATCH end > \$(date)\"
 "
+        if [[ $DEMO_MODE == true ]]; then
+            echo "DEMO: pretending to create new SLURM job ${job_num}-of-${total_jobs}"
+            echo "DEMO: output path | ${slurm_job_file}"
+            echo "DEMO: SLURM job contents..."
+            echo "${sbatch_header}${sbatch_contents}"
+            echo "------------------------------------------------------"
+        else
+            echo "INFO: writing new SLURM job | ${job_num}-of-${total_jobs}"
+            echo "INFO: output path | ${slurm_job_file}"
+            echo "${sbatch_header}${sbatch_contents}" > $slurm_job_file
+        fi
+    else
+        echo "INFO: found existing SLURM job | ${slurm_job_file}"
+    fi
 
-# echo "INFO: automated SLURM job creation"
-# echo "${sbatch_header}
-# ${sbatch_contents}"
+    # Submit the SBATCH file
+    if [ -f $slurm_job_file ]; then
+        if [[ $DEMO_MODE == true ]]; then
+            echo "DEMO: pretending to submit SLURM job ${job_num}-of-${total_jobs} | ${slurm_job_file}"
+        else
+            echo "INFO: submitting SLURM job ${job_num}-of-${total_jobs} | ${slurm_job_file}" 
+            # sbatch $slurm_job_file
+            # sleep 0.3
+        fi
+    fi
+done
 
-# Write the SBATCH file
-slurm_job_file="${jobs_dir}/demo_sbatch1.sh"
-if [ ! -f $slurm_job_file ]; then
-    echo "INFO: creating SLURM job | ${slurm_job_file}"
-    echo "${sbatch_header}${sbatch_contents}" > $slurm_job_file
-else
-    echo "INFO: found existing SLURM job | ${slurm_job_file}"
-fi
 
-# Submit the SBATCH file
-if [ -f $slurm_job_file ]; then
-    sbatch $slurm_job_file
-    sleep 0.3
-fi
+
+
+
+
 
 
 echo "=== experiments.sh end > $(date)"
